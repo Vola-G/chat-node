8ed35cb881a35f4fff628ac0ee670e76c9aaa2c5
Volha Hrakholskaya <volha_hrakholskaya@epam.com>
2020-02-06 17:08:17 +0300

    feat:  add message settings

--- index.js
+++ index.js
@@ -5,19 +5,27 @@ const io = require('socket.io').listen(server);
 
 server.listen(3000);
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
     response.sendFile(__dirname + '/index.html');
 });
 
 users = [];
 connections = [];
 
-io.sockets.on('connection', function(socket){
+io.sockets.on('connection', function (socket) {
     console.log('CONNECTED');
     connections.push(socket);
 
-    socket.on('disconnect', function(data) {
+    socket.on('disconnect', function (data) {
         connections.splice(connections.indexOf(socket), 1);
         console.log('DISCONNECTED');
+    });
+
+    socket.on('send', function (data) {
+        io.sockets.emit('addMsg', {
+            msg: data.mess,
+            name: data.name,
+            className: data.className
+        });
     });
 });
